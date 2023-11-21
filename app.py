6d08324c1e2db49dfc2ef64933860a35ccec105a
ShekharRedd <shekharreddy4558@gmail.com>
2023-11-21 21:09:52 +0530

    remove debug

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def view_tasks():
     return render_template('view_tasks.html', tasks=tasks, now=now)
 
 if __name__ == '__main__':
-    app.run(port=5004,host='0.0.0.0',debug=True)
+    app.run(port=5004,host='0.0.0.0')
